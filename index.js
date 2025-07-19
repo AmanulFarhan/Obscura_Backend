4db1b0c7b251ef3910189572d88f76b79aab1932
AmanulFarhan <amanulfarhan2005@gmail.com>
2025-07-19 00:48:47 -0700

    updated folder structure

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import userRoutes from "./src/routes/user.js"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use("/user", userRoutes);
 
 app.get('/', (req, res) => {
     res.json({
